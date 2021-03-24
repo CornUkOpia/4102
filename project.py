36149187a0dfcdec36c970f74d56e3e611110d5f
Alexander Cornish <46353834+CornUkOpia@users.noreply.github.com>
2021-03-23 21:43:52 -0400

    Add files via upload

---
+++ project.py
@@ -0,0 +1,52 @@
+import numpy as np
+import cv2
+import copy
+from matplotlib import pyplot as plt
+def featuringMatchingBetweenImages(img1,img2,index):
+    detector = cv2.ORB_create()
+    kp1, des1 = detector.detectAndCompute(img1,None)
+    kp2, des2 = detector.detectAndCompute(img2,None)
+
+    # create BFMatcher object
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    # Match descriptors.
+    matches = bf.match(des1,des2)
+
+    # Sort them in the order of their distance.
+    matches = sorted(matches, key = lambda x:x.distance)
+    print(len(matches))
+    img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:20],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    finalTitle = str(i) + "-" + str(i + 1) + ".jpg"
+    cv2.imwrite(finalTitle,img3)
+
+
+for i in range(0,14):
+    title1Str = str(i) + ".jpg"
+    img1 = cv2.imread(title1Str,0)     
+    scale_percent = 50
+    width = int(img1.shape[1] * scale_percent / 100)
+    height = int(img1.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+
+    # resize image
+    img1 = cv2.resize(img1, dim, interpolation = cv2.INTER_AREA)
+    img1 = cv2.equalizeHist(img1)
+    
+    img1 = cv2.GaussianBlur(img1,(5,5),0)
+    title2Str = str(i + 1) + ".jpg"
+    img2 = cv2.imread(title2Str,0)
+    scale_percent = 50
+    width = int(img2.shape[1] * scale_percent / 100)
+    height = int(img2.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+    
+    # resize image
+    img2 = cv2.resize(img2, dim, interpolation = cv2.INTER_AREA)
+    img2 = cv2.equalizeHist(img2)
+    
+    img2 = cv2.GaussianBlur(img2,(5,5),0)
+    
+    featuringMatchingBetweenImages(img1,img2,i)
