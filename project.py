86cb81198c3864abdc1b9fb8d07b315795c0ba36
Alexander Cornish <46353834+CornUkOpia@users.noreply.github.com>
2021-04-06 19:17:37 -0400

    Update project.py

--- project.py
+++ project.py
@@ -4,7 +4,6 @@ import copy
 from matplotlib import pyplot as plt
 import os
 import glob
-
 # The phone camera used is an iPhone 12, the default focal-length is 26mm
 focal_length = -26
 
@@ -27,13 +26,14 @@ def featuringMatchingBetweenImages(img1,img2,index):
     # Sort them in the order of their distance.
     matches = sorted(matches, key = lambda x:x.distance)
     
-    img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:20],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:200],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
     finalTitle = "featureMatched/" + str(i) + "-" + str((i + 1) % len(os.listdir("outputImages"))) + ".jpg"
     cv2.imwrite(finalTitle,img3)
-    src_pts = np.float32([ kp1[matches[m].queryIdx].pt for m in range(0, 20) ]).reshape(-1,1,2)
-    dst_pts = np.float32([ kp2[matches[m].trainIdx].pt for m in range(0, 20) ]).reshape(-1,1,2)
+    src_pts = np.float32([ kp1[matches[m].queryIdx].pt for m in range(0, 200) ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[matches[m].trainIdx].pt for m in range(0, 200) ]).reshape(-1,1,2)
 
     return src_pts, dst_pts
+
 
 def compute_camera_matrix(image1,image2):
     kp1, des1 = detector.detectAndCompute(image1,None)
@@ -47,10 +47,10 @@ def compute_camera_matrix(image1,image2):
     src_pts = np.asarray(src_pts)
     dst_pts = np.asarray(dst_pts)
     F, mask = cv2.findFundamentalMat(src_pts,dst_pts,cv2.FM_RANSAC)
-    return  np.asanyarray(F,np.float32)
+    return  np.asanyarray(F,np.float32), mask
 
 detector = cv2.ORB_create()
-K = compute_camera_matrix(cv2.imread("outputImages/0.jpg",0),cv2.imread("outputImages/1.jpg",0))  
+K = [[1022.5,0,273.648],[0,1004.33,618.487],[0,0,1]]
 if not os.path.exists('outputImages'):
     os.makedirs('outputImages')
 else:
@@ -78,13 +78,19 @@ while(True):
 inputVideo.release()
 
 cv2.destroyAllWindows()
-for i in range(0,1):
-#for i in range(0,len(os.listdir("outputImages"))):
+#for i in range(0,1):
+    #identityMatrix = np.array([[1,0,0,0], [0,1,0,0], [0,0,1,0]])
+   # emptyMatrix = np.empty((3,4))
+for i in range(0,len(os.listdir("outputImages"))):
     title1Str = "outputImages/" + str(i) + ".jpg"
+    title2Str = "outputImages/" + str((i + 1) % len(os.listdir("outputImages"))) + ".jpg"
+    K, mask = compute_camera_matrix(cv2.imread(title1Str,0),cv2.imread(title2Str,0))  
+    print(title1Str,title2Str)
+    
     img1 = cv2.imread(title1Str,-1)     
     img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-    img1 = runImageThroughCameraCalibration(img1)
-
+    
+  
     """scale_percent = 50
     width = int(img1.shape[1] * scale_percent / 100)
     height = int(img1.shape[0] * scale_percent / 100)
@@ -93,11 +99,21 @@ for i in range(0,1):
     
     # resize image
     img1 = cv2.resize(img1, dim, interpolation = cv2.INTER_AREA)"""
-    title2Str = "outputImages/" + str((i + 1) % len(os.listdir("outputImages"))) + ".jpg"
+    
     img2 = cv2.imread(title2Str,1)
     img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-    img2 = runImageThroughCameraCalibration(img2)
+    
     img1Points, img2Points = featuringMatchingBetweenImages(img1,img2,i)
     essentialMatrix, otherThingy = cv2.findEssentialMat(img1Points,img2Points,K)
-    #print(essentialMatrix)
-    print(np.linalg.svd(essentialMatrix))
+    if essentialMatrix is None:
+        continue;
+    else:
+        retval, R, t, mask = cv2.recoverPose(essentialMatrix,img1Points,img2Points,K)
+        print("retval")
+        print(retval)
+        print("R")
+        print(R)
+        print("t")
+        print(t)
+        print("mask")
+        print(mask)
