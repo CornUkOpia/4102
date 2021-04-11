d6f3c00921bb5b11a15c683028036a9fa33e1c33
Alexander Cornish <46353834+CornUkOpia@users.noreply.github.com>
2021-04-11 15:05:26 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,73 +1,41 @@
-import numpy as np
+from PIL import Image, ImageSequence
 import cv2
-import copy
-import matplotlib as mpl
-from matplotlib import pyplot as plt
 import os
-import glob
-# The phone camera used is an iPhone 12, the default focal-length is 26mm
-focal_length = -26
-
-def runImageThroughCameraCalibration(inputImg):  
-   
-    return cv2.filter2D(inputImg,0,K)
-
+import numpy as np
+from matplotlib import pyplot as plt
 
 def featuringMatchingBetweenImages(img1,img2,index):
-   
+    print(index)
     kp1, des1 = detector.detectAndCompute(img1,None)
     kp2, des2 = detector.detectAndCompute(img2,None)
 
     # create BFMatcher object
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
+ 
     # Match descriptors.
     matches = bf.match(des1,des2)
 
     # Sort them in the order of their distance.
     matches = sorted(matches, key = lambda x:x.distance)
     
-    img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:100],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    finalTitle = "featureMatched/" + str(i) + "-" + str((i + 1) % len(os.listdir("outputImages"))) + ".jpg"
+    img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    finalTitle = "featureMatched/" + str(index) + "-" + str((index + 1) % len(os.listdir("outputImages"))) + ".jpg"
+  
     cv2.imwrite(finalTitle,img3)
-    src_pts = np.float32([ kp1[matches[m].queryIdx].pt for m in range(0, 100) ]).reshape(-1,1,2)
-    dst_pts = np.float32([ kp2[matches[m].trainIdx].pt for m in range(0, 100) ]).reshape(-1,1,2)
+    src_pts = np.float32([ kp1[matches[m].queryIdx].pt for m in range(0, len(matches)) ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[matches[m].trainIdx].pt for m in range(0, len(matches)) ]).reshape(-1,1,2)
 
     return src_pts, dst_pts
 
-
-def compute_camera_matrix(image1,image2):
-    kp1, des1 = detector.detectAndCompute(image1,None)
-    kp2, des2 = detector.detectAndCompute(image2,None)
-    # create BFMatcher object
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    matches = bf.match(des1,des2)
-    matches = sorted(matches, key = lambda x:x.distance)
-    src_pts = np.float32([ kp1[matches[m].queryIdx].pt for m in range(0, 100) ]).reshape(-1,1,2)
-    dst_pts = np.float32([ kp2[matches[m].trainIdx].pt for m in range(0, 100) ]).reshape(-1,1,2)
-    src_pts = np.asarray(src_pts)
-    dst_pts = np.asarray(dst_pts)
-    F, mask = cv2.findFundamentalMat(src_pts,dst_pts,cv2.FM_RANSAC)
-    return  np.asanyarray(F,np.float32), mask
-
 detector = cv2.ORB_create()
-K = [[1022.5,0,273.648],[0,1004.33,618.487],[0,0,1]]
-K = np.asanyarray(K, np.float32)
-distCoeffs = [[0.291851, -2.51515, -0.0582267, 0.0035612, 6.9084575]]
-distCoeffs = np.asanyarray(distCoeffs, np.float32)
-
-if not os.path.exists('outputImages'):
-    os.makedirs('outputImages')
-else:
-    files = glob.glob("/outputImages")
-    for f in files:
-        os.remove(f)
-if not os.path.exists('featureMatched'):
-    os.makedirs('featureMatched')
-else:
-    files = glob.glob("/featureMatched")
-    for f in files:
-        os.remove(f)
+"""
+videoSource = Image.open("./cubeSpin.gif")
+i = 0
+for frame in ImageSequence.Iterator(videoSource):
+    frame.save("outputImages/"+str(i)+".png",format = "png", lossless = True)
+    i += 1
+"""
 inputVideo = cv2.VideoCapture("20210331_222940.mp4")
 currentFrame = 0
 frameInterval = 5
@@ -75,102 +43,66 @@ while(True):
     ret,frame = inputVideo.read()
     if ret:
         if (currentFrame % frameInterval) == 0:
-            name = "outputImages/" + str(int(currentFrame/frameInterval)) + ".jpg"
+            name = "outputImages/" + str(int(currentFrame/frameInterval)) + ".png"
             cv2.imwrite(name, frame)
         currentFrame += 1
     else:
         break
 inputVideo.release()
 
-cv2.destroyAllWindows()
-#for i in range(0,1):
-    #identityMatrix = np.array([[1,0,0,0], [0,1,0,0], [0,0,1,0]])
-   # emptyMatrix = np.empty((3,4))
-allPointsX = []
-allPointsY = []
-allPointsZ = []
-R_t_0 = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0]])	# FIXME: Taken from existing code, just as a test. Recreate this.
-R_t_1 = np.empty((3,4))					# FIXME: Taken from existing code, just as a test. Recreate this.
-ProjectionMatrix1 = np.matmul(K, R_t_0)			# FIXME: Taken from existing code, just as a test. Recreate this.
-ProjectionMatrix2 = np.empty((3,4))			# FIXME: Taken from existing code, just as a test. Recreate this.
+
+cameraMatrix = np.asarray([[1022.5,0,273.648],[0,1004.33,618.487],[0,0,1]])
+initial1 = cv2.imread("outputImages/0.png",0)
+initial2 = cv2.imread("outputImages/2.png",0)
+initialPoints1, initialPoints2 = featuringMatchingBetweenImages(initial1,initial2,0)
+initialPoints1 = np.int32(initialPoints1)
+initialPoints2 = np.int32(initialPoints2)
+fundamentalMat, mask = cv2.findFundamentalMat(initialPoints1,initialPoints2,cv2.FM_RANSAC)
+
+initialPoints1 = initialPoints1[mask.ravel() ==1]
+initialPoints2 = initialPoints2[mask.ravel() ==1]
+essentialMat = np.linalg.multi_dot([np.transpose(cameraMatrix),fundamentalMat,cameraMatrix])
+points, R, t, mask = cv2. recoverPose(essentialMat,initialPoints1,initialPoints2)
+M_r = np.hstack((R, t))
+M_l = np.hstack((np.eye(3, 3), np.zeros((3, 1))))
+
+P_l = np.dot(cameraMatrix,  M_l)
+P_r = np.dot(cameraMatrix,  M_r)
+
+# undistort points
+
+
+initialPoints1 = np.squeeze(initialPoints1)
+initialPoints2 = np.squeeze(initialPoints2)
+
+#triangulate points this requires points in normalized coordinate
+point_4d_hom = cv2.triangulatePoints(P_l, P_r, initialPoints1.T, initialPoints2.T)
+print(point_4d_hom)
+
+point_3d = point_4d_hom / np.tile(point_4d_hom[-1, :], (4, 1))
+point_3d = point_3d[:3, :].T
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+for x, y, z in point_3d:
+    ax.scatter(x, y, z, c="r", marker="o")
+
+plt.show()
+fig.savefig('3-D_0.jpg')
+"""
 for i in range(0,len(os.listdir("outputImages"))):
-    title1Str = "outputImages/" + str(i) + ".jpg"
-    title2Str = "outputImages/" + str((i + 1) % len(os.listdir("outputImages"))) + ".jpg"
-    F, mask = compute_camera_matrix(cv2.imread(title1Str,0),cv2.imread(title2Str,0))  
+    title1Str = "outputImages/" + str(i) + ".png"
+    title2Str = "outputImages/" + str((i + 1) % len(os.listdir("outputImages"))) + ".png"
+    fundamental, mask = compute_camera_matrix(cv2.imread(title1Str,0),cv2.imread(title2Str,0))  
     print(title1Str,title2Str)
     
     img1 = cv2.imread(title1Str,-1)     
-    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-    
-  
-    """scale_percent = 50
-    width = int(img1.shape[1] * scale_percent / 100)
-    height = int(img1.shape[0] * scale_percent / 100)
-    dim = (width, height)
-    
-    
-    # resize image
-    img1 = cv2.resize(img1, dim, interpolation = cv2.INTER_AREA)"""
-    
-    img2 = cv2.imread(title2Str,1)
-    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+    img2 = cv2.imread(title2Str,-1)
+
     
     img1Points, img2Points = featuringMatchingBetweenImages(img1,img2,i)
-    # Filter the outliers.
-    for j in range(len(img1Points)):
-        img1Points[j] = img1Points[j] if mask[j].ravel() == 1 else 0
-    for j in range(len(img2Points)):
-        img2Points[j] = img2Points[j] if mask[j].ravel() == 1 else 0
-    essentialMatrix, otherThingy = cv2.findEssentialMat(img1Points,img2Points,K)
-    if essentialMatrix is None:
-        continue;
-    else:
-        retval, R, t, mask = cv2.recoverPose(essentialMatrix,img1Points,img2Points,K)
-
-        undistorted1 = cv2.undistortPoints(img1Points,K,distCoeffs=distCoeffs)
-        undistorted2 = cv2.undistortPoints(img2Points,K,distCoeffs=distCoeffs)
-
-        #ProjectionMatrix1 = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0]])
-        #ProjectionMatrix2 = np.concatenate((np.dot(K,R),np.dot(K,t)),axis = 1)
-        # FIXME: Taken from existing code, just as a test. Recreate this.
-        R_t_1[:3,:3] = np.matmul(R, R_t_0[:3,:3])
-        R_t_1[:3, 3] = R_t_0[:3, 3] + np.matmul(R_t_0[:3,:3], t.ravel())
-        R_t_0 = np.copy(R_t_1)
-        ProjectionMatrix2 = np.matmul(K, R_t_1)
-       
-        triangulatedPoints = cv2.triangulatePoints(ProjectionMatrix1,ProjectionMatrix2,undistorted1,undistorted2)
-        #print(triangulatedPoints[3])
-        #triangulatedPoints /= triangulatedPoints[3]
-
-        ProjectionMatrix1 = np.copy(ProjectionMatrix2)
-
-        print("___________________________________")
-        allPointsX.append(triangulatedPoints[0])
-        allPointsY.append(triangulatedPoints[1])
-        allPointsZ.append(triangulatedPoints[2])
-        #print(triangulatedPoints.shape)
-        #print(len(triangulatedPoints))
-        #print(triangulatedPoints)
-        fig = plt.figure()
-        ax = plt.axes(projection="3d")
-        #ax.set_autoscale_on(False)
-        #ax.set_xlim3d([0.0, 0.5])
-        #ax.set_zlim3d([0.45, 0.5])
-        #ax.set_ylim3d([-0.86, 0.00005])
-        ax.set_xlabel("X")
-        ax.set_ylabel("Z")
-        ax.set_zlabel("Y")
-        ax.scatter3D(triangulatedPoints[0], triangulatedPoints[2], triangulatedPoints[1])
-        plt.savefig("graphs/temp"+ str(i) +".png")
-        plt.close()
-fig = plt.figure()
-ax = plt.axes(projection="3d")
-ax.set_xlim3d([np.average(allPointsX) - 2*np.std(allPointsX), np.average(allPointsX) + 2*np.std(allPointsX)])
-ax.set_ylim3d([np.average(allPointsY) - 2*np.std(allPointsY), np.average(allPointsY) + 2*np.std(allPointsY)])
-ax.set_zlim3d([np.average(allPointsZ) - 2*np.std(allPointsZ), np.average(allPointsZ) + 2*np.std(allPointsZ)])
-ax.set_xlabel("X")
-ax.set_ylabel("Z")
-ax.set_zlabel("Y")
-ax.scatter3D(allPointsX, allPointsZ, allPointsY)
-plt.savefig("./pointcloud.png")
-plt.close()
+"""
